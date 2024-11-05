c1d6c8876743830a9bb4f1dcdcdfecba85d2b160
Abhishek <abhishek.websenor@gmail.com>
2024-11-05 17:47:52 +0530

    tyddfqwtyd

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from "./app.js"
 import { connectDB } from "./config/database.js"
-import cloudinary from "cloudinary"
+// import cloudinary from "cloudinary"
 import RazorPay from "razorpay"
 import nodeCron from "node-cron"
 import { Stats } from "./models/Stats.js"
@@ -23,11 +23,11 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 
 connectDB()
 
-cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
-    api_key: process.env.CLOUDINARY_CLIENT_API,
-    api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
-})
+// cloudinary.v2.config({
+//     cloud_name: process.env.CLOUDINARY_CLIENT_NAME,
+//     api_key: process.env.CLOUDINARY_CLIENT_API,
+//     api_secret: process.env.CLOUDINARY_CLIENT_SECRET,
+// })
 
 export const instance = new RazorPay({
     key_id: "rzp_test_cSQRF8g2iWxvt7",
